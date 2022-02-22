c5f02d1e1d9f687bb9e5bc21d1781836c0c97e15
Reality Failure <47836504+VaingloryOP@users.noreply.github.com>
2022-02-21 18:47:18 -0600

    Update App.js
    So far, this contains the login page and its basic components except for a possible button that will lead to an account creation page.
    This is the first page that will be displayed when the app is opened. All it does for now, is print "You have entered the correct name and password X times.", every time the enter button is pressed with "hello" entered as the username, and "goodbye" entered as the password, X being a variable that is incremented with every press.

--- App.js
+++ App.js
@@ -1,31 +1,69 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Button } from "react-native";
+import React, { useState } from 'react';
+import { SafeAreaView, StyleSheet, Text, TextInput, View, Image, Button} from 'react-native';
 
-export default function App() {
-    const handleClick = () => {
-        console.log("clicked");
-    };
+var correctAttempts = 0;
 
-    return (
-        <View style={styles.container}>
-            <Text>Open up App.js to start working on your app!</Text>
-            <View style={{ backgroundColor: "#ff0000" }}>
-                <Text style={styles.text}>Dumb text</Text>
-            </View>
-            <Button title='button' onClick={handleClick} />
-            <StatusBar style='auto' />
-        </View>
-    );
+const SoundBytes = () => {
+
+  const [username, setUsername] = useState('Empty_Username_Field')
+  const [password, setPassword] = useState('Empty_Password_Field')
+
+ 
+  
+  return(
+  <SafeAreaView style={styles.container}>
+
+    <Text style={styles.textContainer}>Welcome to SoundBytes</Text>
+
+    {/*The LSU Tiger background images on the login page*/}
+    <Image source={{
+      width: 350,
+      height: 350,
+      uri: "https://bossierpress.com/wp-content/uploads/2016/05/lsutigerslogo2.jpg"
+      }}/>
+      
+      <View style={{position: 'absolute', top: -90, left: 0, right: 0, bottom: 0, justifyContent: 'center', alignItems: 'center'}}>
+      <TextInput style={{height: 60, width: 320, margin: 12, borderWidth: 3, padding: 10, fontSize: 24, color: 'red'}}
+      placeholder= "Username"
+      onChangeText={(username) => setUsername(username)}
+      />
+      <TextInput style={{height: 60, width: 320, margin: 12, borderWidth: 3, padding: 10, fontSize: 24, color: 'red'}}
+      placeholder= "Password"
+      onChangeText={(password) => setPassword(password)}
+      />
+      {/*This 'enter' button will take in the inputted username and password and check the database for a profile*/}
+      <Button onPress={() => Validate(username, password)} title="Enter"/>
+      </View>
+
+  </SafeAreaView>
+  )
+}
+
+{/*Called upon pressing the enter button on the login page.
+  TODO: Check if the username and password has a match to a user profile in the database.
+*/}
+const Validate = (username: string, password: string) =>{
+  if(username.localeCompare("hello") == 0 && password.localeCompare("goodbye") == 0){
+    console.log("You entered the right name and password " + correctAttempts + " times")
+    correctAttempts = correctAttempts + 1
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#fff",
-        alignItems: "center",
-        justifyContent: "center",
-    },
-    text: {
-        color: "#fff",
-    },
-});
+
+  container:{
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#61E5F9'
+  },
+  textContainer:{
+    fontSize: 24,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'blue'
+  }
+})
+
+
+export default SoundBytes;
