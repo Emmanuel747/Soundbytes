efefd0e4d7bdcf9c5b013c3d07977b7080cf4a80
Zeke Abshire <zekeabshire@gmail.com>
2022-02-27 21:42:37 -0600

    added firebase to the project

--- src/backend/Fire.ts
+++ src/backend/Fire.ts
@@ -1,4 +1,20 @@
-// Temporary substitution for Firebase
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAJ9HdGf3c7Y-jK1CDWQPtmHR8vHzwfgpo",
+    authDomain: "notify-soundbytes.firebaseapp.com",
+    projectId: "notify-soundbytes",
+    storageBucket: "notify-soundbytes.appspot.com",
+    messagingSenderId: "421047656428",
+    appId: "1:421047656428:web:4dd7687cba51cf7eb76ac9",
+    measurementId: "G-FDMLBBMXM2",
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+// Temporary Substitution for Firebase ----------------------------------------
 
 class FireAuth implements IAuthenticator {
     authenticateUser(username: String, password: String): void {
