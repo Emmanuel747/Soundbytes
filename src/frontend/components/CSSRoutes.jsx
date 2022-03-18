6d27a0bb059dc8f029dde5d4a4e046545568d342
Emmanuel <EmmanuelEEjim@gmail.com>
2022-03-17 19:34:50 -0500

    Fixed the Feed, Post and style errs. Merge rdy

---
+++ src/frontend/components/CSSRoutes.jsx
@@ -0,0 +1,3 @@
+import PostStyles from '../components/PostStyles.jsx';
+
+export default PostStyles;
--- src/frontend/components/Post.jsx
+++ src/frontend/components/Post.jsx
@@ -1,39 +1,36 @@
 import { FlatList, ScrollView, View, Text, Image } from "react-native";
-import Icon from "react-native-vector-icons/AntDesign";
-
 import { GlobalFeedComposer, PostManager } from "../../backend";
-import PostStyles from "./PostStyles";
+import AntDesignIcon from "react-native-vector-icons/AntDesign";
+import PostStyles from './CSSRoutes';
 
-export default function Post({ userName, profileImg, msgContent }) {
-    const playIcon = (
-        <Icon
-            style={PostStyles.icon}
-            name='playcircleo'
-            size={60}
-            color='blue'
-        />
-    );
+export default function Post({ userName, profileImg, msgContent, i }) {
+  const playIcon = <AntDesignIcon
+    style={PostStyles.icon}
+    name='playcircleo'
+    size={60}
+    color='blue'
+  />;
 
-    const feedComposer = new GlobalFeedComposer();
-    const postManager = new PostManager();
+  const feedComposer = new GlobalFeedComposer();
+  const postManager = new PostManager();
 
-    const getFeed = () => {
-        // Call feedComposer.composeFeed()
-        throw new Error("Method not implemented.");
-    };
-    const commentOnPost = () => {
-        // Redirect to Recording page
-        throw new Error("Method not implemented.");
-    };
-    const likePost = () => {
-        // Call postManager.updatePost
-        throw new Error("Method not implemented.");
-    };
+  const getFeed = () => {
+    // Call feedComposer.composeFeed()
+    throw new Error("Method not implemented.");
+  };
+  const commentOnPost = () => {
+    // Redirect to Recording page
+    throw new Error("Method not implemented.");
+  };
+  const likePost = () => {
+    // Call postManager.updatePost
+    throw new Error("Method not implemented.");
+  };
 
-    return (
-        <View key={i} style={PostStyles.subContainer}>
-            {playIcon}
-            <Text>"Sleep is for the weak " {i}</Text>
-        </View>
-    );
+  return (
+    <View key={i} style={PostStyles.subContainer}>
+      {playIcon}
+      <Text>"Sleep is for the weak " {i}</Text>
+    </View>
+  );
 }
