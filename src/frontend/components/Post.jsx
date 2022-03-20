a51d14a07f49e578543e1b28075810f2daebef45
Zeke Abshire <zekeabshire@gmail.com>
2022-03-19 20:19:48 -0500

    formatted w/ Prettier

--- src/frontend/components/Post.jsx
+++ src/frontend/components/Post.jsx
@@ -1,70 +1,69 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import { View, Text, Image, Pressable } from "react-native";
 import { GlobalFeedComposer, PostManager } from "../../backend";
 import AntDesignIcon from "react-native-vector-icons/AntDesign";
-import PostStyles from './StyleSheets/PostStyles.jsx';
-import BtnStyles from './StyleSheets/BtnStyles.jsx';
+import PostStyles from "./StyleSheets/PostStyles.jsx";
+import BtnStyles from "./StyleSheets/BtnStyles.jsx";
 
 export default function Post({ userName, profileImg, msgContent, i }) {
+    const [isPlaying, setPlayingStatus] = useState(true);
+    const playOrPause = () => {
+        if (isPlaying === true) {
+            setPlayingStatus(false);
+        } else setPlayingStatus(true);
+    };
 
-  const [isPlaying, setPlayingStatus] = useState(true);
-  const playOrPause = () => {
-    if (isPlaying === true) {
-      setPlayingStatus(false)
-    } else setPlayingStatus(true)
-  }
+    const playIcon = (
+        <Pressable
+            onPress={() => {
+                playOrPause();
+            }}
+            style={({ pressed }) => [
+                {
+                    backgroundColor: pressed ? "#b8ab67" : "rgb(46, 66, 219)",
+                },
+                BtnStyles.btnWrapper,
+            ]}>
+            {({ pressed }) => (
+                <Text style={styles.text}>
+                    {pressed ? "Pressed!" : "Press Me"}
+                </Text>
+            )}
+            <AntDesignIcon
+                style={BtnStyles.icon}
+                name='playcircleo'
+                size={40}
+                color='rgb(46, 66, 219)'
+            />
+        </Pressable>
+    );
+    const feedComposer = new GlobalFeedComposer();
+    const postManager = new PostManager();
 
-  const playIcon = 
-    <Pressable
-      onPress={() => {
-        playOrPause();
-      }}
-      style={({ pressed }) => [
-        {
-          backgroundColor: pressed
-            ? '#b8ab67'
-            : 'rgb(46, 66, 219)'
-        },
-        BtnStyles.btnWrapper
-      ]}>
-      {({ pressed }) => (
-        <Text style={styles.text}>
-          {pressed ? 'Pressed!' : 'Press Me'}
-        </Text>
-      )}
-      <AntDesignIcon
-        style={BtnStyles.icon}
-        name='playcircleo'
-        size={40}
-        color='rgb(46, 66, 219)'
-      />
-    </Pressable>
-    ;
+    const getFeed = () => {
+        // Call feedComposer.composeFeed()
+        throw new Error("Method not implemented.");
+    };
 
-  const feedComposer = new GlobalFeedComposer();
-  const postManager = new PostManager();
+    const commentOnPost = () => {
+        // Redirect to Recording page
+        throw new Error("Method not implemented.");
+    };
+    const likePost = () => {
+        // Call postManager.updatePost
+        throw new Error("Method not implemented.");
+    };
 
-  const getFeed = () => {
-    // Call feedComposer.composeFeed()
-    throw new Error("Method not implemented.");
-  };
-  
-  const commentOnPost = () => {
-    // Redirect to Recording page
-    throw new Error("Method not implemented.");
-  };
-  const likePost = () => {
-    // Call postManager.updatePost
-    throw new Error("Method not implemented.");
-  };
-
-  return (
-    <>
-      <View style={PostStyles.subContainer}>
-        {playIcon}
-        <Text> {msgContent} {i} </Text>
-        {profileImg}
-      </View>
-    </>
-  );
+    return (
+        <>
+            <View style={PostStyles.subContainer}>
+                {playIcon}
+                <Text>
+                    {" "}
+                    {msgContent} {i}{" "}
+                </Text>
+                {profileImg}
+            </View>
+        </>
+    );
 }
--- src/frontend/components/PressableBtn.jsx
+++ src/frontend/components/PressableBtn.jsx
@@ -1,7 +1,6 @@
 // import React from "react"
 // import { Pressable, PressableProps, StyleProp, ViewStyle } from "react-native"
 
-
 // const PressableBtn = ({ scale, style, children, ...otherProps }) => {
 //   return (
 //     <Pressable style={({ pressed }) => [style, { transform: [{ scale: pressed ? (scale ?? 0.98) : 1 }] }]} {...otherProps}>
