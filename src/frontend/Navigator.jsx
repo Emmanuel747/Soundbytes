2a70ad98cfcb1994d50cc2ee3ecc43fd2f4c15c1
Reality Failure <Kevynjmalveaux@hotmail.com>
2022-03-22 10:00:16 -0500

    Fixed an error in file pathing

--- src/frontend/Navigator.jsx
+++ src/frontend/Navigator.jsx
@@ -5,10 +5,10 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 import Icon from "react-native-vector-icons/FontAwesome5";
 
-import LoginScreen from "./screens/LoginScreen";
-import FeedScreen from "./screens/FeedScreen";
-import ProfileScreen from "./screens/ProfileScreen";
-import RecordingScreen from "./screens/RecordingScreen";
+import LoginScreen from "./Screens/LoginScreen";
+import FeedScreen from "./Screens/FeedScreen";
+import ProfileScreen from "./Screens/ProfileScreen";
+import RecordingScreen from "./Screens/RecordingScreen";
 
 import colors from "./colors";
 
@@ -20,116 +20,111 @@ import colors from "./colors";
 const Tab = createBottomTabNavigator();
 
 const Screens = {
-    Feed: {
-        name: "Feed",
-        component: FeedScreen,
-        icons: {
-            normal: "list-ul",
-            focused: "list-ul",
-        },
+  Feed: {
+    name: "Feed",
+    component: FeedScreen,
+    icons: {
+      normal: "list-ul",
+      focused: "list-ul",
     },
-    Recording: {
-        name: "Recording",
-        component: RecordingScreen,
-        options: {
-            tabBarIcon: ({ focused, color, size }) => (
-                <RecordButton focused={focused} color={color} size={size} />
-            ),
-        },
-        icons: {
-            normal: "microphone",
-            focused: "microphone",
-        },
+  },
+  Recording: {
+    name: "Recording",
+    component: RecordingScreen,
+    options: {
+      tabBarIcon: ({ focused, color, size }) => (
+        <RecordButton focused={focused} color={color} size={size} />
+      ),
     },
-    Profile: {
-        name: "Profile",
-        component: ProfileScreen,
-        options: {},
-        icons: {
-            normal: "user-alt",
-            focused: "user-alt",
-        },
+    icons: {
+      normal: "microphone",
+      focused: "microphone",
     },
+  },
+  Profile: {
+    name: "Profile",
+    component: ProfileScreen,
+    options: {},
+    icons: {
+      normal: "user-alt",
+      focused: "user-alt",
+    },
+  },
 };
 
 const RecordButton = ({ focused, color, size }) => {
-    return (
-        <View
-            style={{
-                backgroundColor: focused ? "white" : colors.primary,
-                justifyContent: "center",
-                alignContent: "center",
-                alignSelf: "center",
-                alignItems: "center",
-                width: 90,
-                height: 90,
-                elevation: 10,
-                borderRadius: 200,
-            }}>
-            <Icon name='microphone' color={color} size={size * 1.5} />
-        </View>
-    );
+  return (
+    <View
+      style={{
+        backgroundColor: focused ? "white" : colors.primary,
+        justifyContent: "center",
+        alignContent: "center",
+        alignSelf: "center",
+        alignItems: "center",
+        width: 90,
+        height: 90,
+        elevation: 10,
+        borderRadius: 200,
+      }}
+    >
+      <Icon name="microphone" color={color} size={size * 1.5} />
+    </View>
+  );
 };
 
 export default function Navigator() {
-    const [isSignedIn, setIsSignedIn] = useState(false);
+  const [isSignedIn, setIsSignedIn] = useState(false);
 
-    return (
-        <NavigationContainer>
-            <Tab.Navigator
-                initialRouteName={Screens.Feed.name}
-                screenOptions={({ route }) => ({
-                    tabBarHideOnKeyboard: true,
-                    headerTitleAlign: "center",
-                    tabBarActiveTintColor: colors.active,
-                    tabBarInactiveTintColor: colors.disabled,
-                    // headerStyle: {},
-                    // headerShown: false,
-                    tabBarShowLabel: false,
-                    tabBarStyle: {
-                        height: 60,
-                        padding: 0,
-                    },
-                    tabBarIcon: ({ focused, color, size }) => {
-                        let screen = Screens[route.name];
-                        if (screen)
-                            return (
-                                <Icon
-                                    name={
-                                        focused
-                                            ? screen.icons.focused
-                                            : screen.icons.normal
-                                    }
-                                    size={size}
-                                    color={color}
-                                />
-                            );
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName={Screens.Feed.name}
+        screenOptions={({ route }) => ({
+          tabBarHideOnKeyboard: true,
+          headerTitleAlign: "center",
+          tabBarActiveTintColor: colors.active,
+          tabBarInactiveTintColor: colors.disabled,
+          // headerStyle: {},
+          // headerShown: false,
+          tabBarShowLabel: false,
+          tabBarStyle: {
+            height: 60,
+            padding: 0,
+          },
+          tabBarIcon: ({ focused, color, size }) => {
+            let screen = Screens[route.name];
+            if (screen)
+              return (
+                <Icon
+                  name={focused ? screen.icons.focused : screen.icons.normal}
+                  size={size}
+                  color={color}
+                />
+              );
 
-                        // This is for the login page, since it is not defined in Screens
-                        return <Icon name='lock' size={size} color={color} />;
-                    },
-                })}>
-                {isSignedIn ? (
-                    Object.entries(Screens).map(([key, val], i) => (
-                        <Tab.Screen
-                            name={key}
-                            component={val.component}
-                            options={val.options}
-                            key={i} // Need key for rendering lists
-                        />
-                    ))
-                ) : (
-                    <Tab.Screen
-                        name='Login'
-                        children={({ props }) => (
-                            <LoginScreen
-                                {...props}
-                                onSuccess={() => setIsSignedIn(true)}
-                            />
-                        )}
-                    />
-                )}
-            </Tab.Navigator>
-        </NavigationContainer>
-    );
+            // This is for the login page, since it is not defined in Screens
+            return <Icon name="lock" size={size} color={color} />;
+          },
+        })}
+      >
+        {isSignedIn ? (
+          Object.entries(Screens).map(([key, val], i) => (
+            <Tab.Screen
+              name={key}
+              component={val.component}
+              options={val.options}
+              key={i} // Need key for rendering lists
+            />
+          ))
+        ) : (
+          <Tab.Screen
+            name="Login"
+            children={({ props }) => (
+              <LoginScreen {...props} onSuccess={() => setIsSignedIn(true)} />
+            )}
+          />
+        )}
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
 }
