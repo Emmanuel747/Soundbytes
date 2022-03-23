d968928cc2dccf9354b758d9bdc266e3fc4a0c92
Zeke Abshire <zekeabshire@gmail.com>
2022-03-23 01:36:59 -0500

    minor cleanup

--- src/frontend/hooks/useOnce.js
+++ src/frontend/hooks/useOnce.js
@@ -4,7 +4,7 @@ export default function useOnce(Obj) {
     const [instance, setInstance] = useState(Obj);
 
     useEffect(() => {
-        console.log("rerendering", instance.toString());
+        // console.log("rerendering", instance.toString());
 
         if (!instance) setInstance(Obj);
 
