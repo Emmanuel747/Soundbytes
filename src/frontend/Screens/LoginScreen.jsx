e26af0062736a476babebe93f40db0173b3bb0d4
mseruntine <miguelseruntine@gmail.com>
2022-03-23 21:53:33 -0500

    small tweaks

--- src/frontend/Screens/LoginScreen.jsx
+++ src/frontend/Screens/LoginScreen.jsx
@@ -6,124 +6,123 @@ import {
     View,
     Image,
     Button,
-} from "react-native";
-import React, { useState } from "react";
-import { FireAuth, UserManager } from "../../backend";
-
-var correctAttempts = 0;
-
-export default function LoginScreen({ onSuccess }) {
+  } from "react-native";
+  import React, { useState } from "react";
+  import { FireAuth, UserManager } from "../../backend";
+  
+  var correctAttempts = 0;
+  const auth = new FireAuth();
+  
+  export default function LoginScreen({ onSuccess }) {
     const userManager = new UserManager();
-    const auth = new FireAuth();
-
+  
     const [username, setUsername] = useState("Empty_Username_Field");
     const [password, setPassword] = useState("Empty_Password_Field");
-
+  
     return (
-        <SafeAreaView style={styles.container}>
-            <SafeAreaView
-                style={{
-                    width: 350,
-                    height: 380,
-                    borderColor: "#000000",
-                    borderWidth: 2,
-                    borderRadius: 9,
-                    justifyContent: "center",
-                }}>
-                <View
-                    style={{
-                        position: "absolute",
-                        top: -90,
-                        left: 0,
-                        right: 0,
-                        bottom: 0,
-                        justifyContent: "center",
-                        alignItems: "center",
-                    }}>
-                    <Text
-                        style={{
-                            fontSize: 35,
-                            padding: 15,
-                            fontWeight: "bold",
-                            justifyContent: "center",
-                            alignItems: "center",
-                        }}>
-                        ♪ SoundBytes ♪
-                    </Text>
-                    <TextInput
-                        style={{
-                            height: 60,
-                            width: 320,
-                            margin: 12,
-                            borderWidth: 3,
-                            padding: 10,
-                            fontSize: 24,
-                            color: "black",
-                        }}
-                        placeholder='username'
-                        onChangeText={(username) => setUsername(username)}
-                    />
-                    <TextInput
-                        style={{
-                            height: 60,
-                            width: 320,
-                            margin: 12,
-                            borderWidth: 3,
-                            padding: 10,
-                            fontSize: 24,
-                            color: "black",
-                        }}
-                        secureTextEntry={true}
-                        placeholder='password'
-                        onChangeText={(password) => setPassword(password)}
-                    />
-                    {/*This 'enter' button will take in the inputted username and password and check the database for a profile by calling Validate()*/}
-
-                    <Button
-                        onPress={() => {
-                            Validate(username, password);
-                            onSuccess();
-                        }}
-                        title='Login'
-                    />
-                </View>
-            </SafeAreaView>
+      <SafeAreaView style={styles.container}>
+        <SafeAreaView
+          style={{
+            width: 350,
+            height: 380,
+            borderColor: "#000000",
+            borderWidth: 2,
+            borderRadius: 9,
+            ...styles.container,
+          }}
+        >
+          <View
+            style={{
+              position: "absolute",
+              top: -90,
+              left: 0,
+              right: 0,
+              bottom: 0,
+              ...styles.container,
+            }}
+          >
+            <Text
+              style={{
+                fontSize: 35,
+                padding: 15,
+                fontWeight: "bold",
+                ...styles.container,
+              }}
+            >
+              ♪ SoundBytes ♪
+            </Text>
+            <TextInput
+              style={styles.text}
+              placeholder="username"
+              onChangeText={(username) => setUsername(username)}
+            />
+            <TextInput
+              style={styles.text}
+              secureTextEntry={true}
+              placeholder="password"
+              onChangeText={(password) => setPassword(password)}
+            />
+            {/*This 'enter' button will take in the inputted username and password and check the database for a profile by calling Validate()*/}
+  
+            <Button
+              onPress={() => {
+                if (Validate(username, password)) {
+                  onSuccess();
+                }
+              }}
+              title="Login"
+            />
+          </View>
         </SafeAreaView>
+      </SafeAreaView>
     );
-}
-
-{
-    /*Called upon pressing the enter button on the login page.
-  TODO: Check if the username and password has a match to a user profile in the database.
-*/
-}
-const Validate = (username, password) => {
-    if (
-        username.localeCompare("hello") == 0 &&
-        password.localeCompare("goodbye") == 0
-    ) {
-        console.log(
-            "You entered the right name and password " +
-                correctAttempts +
-                " times"
-        );
-        correctAttempts = correctAttempts + 1;
+  }
+  
+  //This function passes the username and password inputs to the firebase authenticator, aka Fire.ts
+  //True authentication not yet implemented.
+  const Validate = (username, password) => {
+    /* if (
+          username.localeCompare("hello") == 0 &&
+          password.localeCompare("goodbye") == 0
+      ) {
+          console.log(
+              "You entered the right name and password " +
+                  correctAttempts +
+                  " times"
+          );
+          correctAttempts = correctAttempts + 1;
+      } */
+  
+    if (auth.authenticateUser(username, password)) {
+      return true;
     }
-};
-
-const styles = StyleSheet.create({
+  };
+  
+  const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        /*   backgroundColor: "#61E5F9", */
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
     },
     textContainer: {
-        fontSize: 34,
+      fontSize: 34,
+      justifyContent: "center",
+      alignItems: "center",
+    },
+    loginScreenStyle: {
         justifyContent: "center",
-        alignItems: "center",
+      alignItems: "center",
     },
     button: {
-        backgroundColor: "black",
+      backgroundColor: "black",
     },
-});
+    text: {
+      height: 60,
+      width: 320,
+      margin: 12,
+      borderWidth: 3,
+      padding: 10,
+      fontSize: 24,
+      color: "black",
+    }
+  });
