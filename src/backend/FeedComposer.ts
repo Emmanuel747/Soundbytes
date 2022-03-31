7712b63c42abcd2864838407b2677f3153a552d8
Zeke Abshire <zekeabshire@gmail.com>
2022-03-31 12:29:47 -0500

    FC complete (havent tested though)

--- src/backend/FeedComposer.ts
+++ src/backend/FeedComposer.ts
@@ -1,82 +1,50 @@
-// TODO
+// Complete
+
+import { Database } from "./storage/Database";
 
 // Abstract Factory Pattern
 
 class AbstractFeedComposer implements IFeedComposer {
-    composeFeed(): Feed {
+    database: IDatabase;
+
+    constructor() {
+        this.database = new Database();
+    }
+
+    composeFeed(): Promise<Feed> {
         throw new Error("Method not implemented.");
     }
 }
 
 class GlobalFeedComposer extends AbstractFeedComposer {
-    composeFeed(): Feed {
-        // Return Database.getAllPosts()
-        return [
-            {
-                title: "Title from Global",
-                creator: "ZekeIsCool",
-                timestamp: "1/1/22 10:42:31",
-                likes: 0,
-            },
-            {
-                title: "Title",
-                creator: "ZekeIsCool",
-                timestamp: "1/1/22 10:42:32",
-                likes: 0,
-            },
-        ] as Feed;
+    async composeFeed(): Promise<Feed> {
+        // Get all posts from the database
+        return (await this.database.getAllPosts()) as Feed;
     }
 }
 
 class LocalFeedComposer extends AbstractFeedComposer {
-    composeFeed(): Feed {
+    uid: string;
+
+    constructor(uid: string) {
+        super();
+        this.uid = uid;
+    }
+
+    async composeFeed(): Promise<Feed> {
         // Get current user's following list: UID[]
-        // from Database.getUser
-        // Return Database.getPostsFromUsers(following)
-        return [
-            {
-                title: "Title from Local",
-                creator: "ZekeIsCool",
-                timestamp: "1/1/22 10:42:33",
-                likes: 0,
-            },
-            {
-                title: "Title",
-                creator: "ZekeIsCool",
-                timestamp: "1/1/22 10:42:34",
-                likes: 0,
-            },
-            {
-                title: "Title",
-                creator: "ZekeIsCool",
-                timestamp: "1/1/22 10:42:35",
-                likes: 0,
-            },
-        ] as Feed;
+        // from Database.getUser, and then return all their posts
+        const following = (await this.database.getUser(this.uid)).following;
+        return this.database.getPostsFromUsers(following);
     }
 }
 
-class ProfileFeedComposer extends AbstractFeedComposer {
-    // Might be necessary
-    // user: string;
-
-    // constructor(user: string) {
-    //     super();
-    //     this.user = user;
-    // }
-
-    composeFeed(): Feed {
+class ProfileFeedComposer extends LocalFeedComposer {
+    async composeFeed(): Promise<Feed> {
         // Return Database.getPostsFromUsers([currentUID])
         // Note: the function expects an array,
         // so just have the one UID in the array
-        return [
-            {
-                title: "Title from Profile",
-                creator: "Zeke",
-                timestamp: "1/1/22 10:42:36",
-                likes: 0,
-            },
-        ] as Feed;
+        return (await this.database.getPostsFromUsers([this.uid])) as Feed;
     }
 }
 
