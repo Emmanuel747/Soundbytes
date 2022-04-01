14d56eef5ea4065561864faa992ce90490f69c11
Reality Failure <Kevynjmalveaux@hotmail.com>
2022-04-01 16:06:11 -0500

    Implemented Authentication

--- src/backend/storage/Database.ts
+++ src/backend/storage/Database.ts
@@ -1,109 +1,110 @@
 // TODO
 
 import {
-    collection,
-    doc,
-    getDoc,
-    getDocs,
-    setDoc,
-    query,
-    updateDoc,
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  setDoc,
+  query,
+  updateDoc,
 } from "firebase/firestore";
 import { FireDB } from "../Fire";
 
 class Database implements IDatabase {
-    async getPost(pid: string): Promise<Post> {
-        // Simply get post by PID
-        const docRef = doc(FireDB, "posts", pid);
-        const docSnap = await getDoc(docRef);
+  async getPost(pid: string): Promise<Post> {
+    // Simply get post by PID
+    const docRef = doc(FireDB, "posts", pid);
+    const docSnap = await getDoc(docRef);
 
-        if (docSnap.exists()) {
-            return docSnap.data() as Post;
-        } else {
-            throw new Error("Method not implemented.");
-        }
+    if (docSnap.exists()) {
+      return docSnap.data() as Post;
+    } else {
+      throw new Error("Method not implemented.");
     }
+  }
 
-    async makePost(post: Post): Promise<void> {
-        // Add a post to the 'posts' collection
-        //   + the 'users' collection (users/UID/post) X
-        const docRef = doc(FireDB, "posts");
-        await setDoc(docRef, post);
-        //WE NEED TO DO THE USERS PART :O
-    }
+  async makePost(post: Post): Promise<void> {
+    // Add a post to the 'posts' collection
+    //   + the 'users' collection (users/UID/post) X
+    const docRef = doc(FireDB, "posts");
+    await setDoc(docRef, post);
+    //WE NEED TO DO THE USERS PART :O
+  }
 
-    async editPost(post: PostEditable, pid: string): Promise<void> {
-        // Update doc with the changes
-        // WARNING: change in bot 'users' and 'posts'
-        // (We have redundant data on purpose)
-        const docRef = doc(FireDB, "posts", pid);
-        this.getPost(pid);
-        throw new Error("Method not implemented.");
-    }
+  async editPost(post: PostEditable, pid: string): Promise<void> {
+    // Update doc with the changes
+    // WARNING: change in bot 'users' and 'posts'
+    // (We have redundant data on purpose)
+    const docRef = doc(FireDB, "posts", pid);
+    this.getPost(pid);
+    throw new Error("Method not implemented.");
+  }
 
-    async getUser(uid: string): Promise<User> {
-        // Simply get user by UID
-        throw new Error("Method not implemented.");
-    }
+  async getUser(uid: string): Promise<User> {
+    // Simply get user by UID
+    throw new Error("Method not implemented.");
+  }
 
-    makeUser(user: User, uid: string): void {
-        // Add a User to the 'users' collection
-        // Add username to the 'usernames' doc in 'users'
-        //
-        // This one will probably be odd because FireAuth stores
-        // some things about the user (like password, uid,
-        // and email) behind the scenes in its own database.
-        // DON'T ADD THE PASSWORD
-        throw new Error("Method not implemented.");
-    }
+  makeUser(user: User, uid: string): void {
+    // Add a User to the 'users' collection
+    // Add username to the 'usernames' doc in 'users'
+    //
+    // This one will probably be odd because FireAuth stores
+    // some things about the user (like password, uid,
+    // and email) behind the scenes in its own database.
+    // DON'T ADD THE PASSWORD
 
-    editUser(user: UserEditable, uid: string): void {
-        // Update doc with the changes
-        throw new Error("Method not implemented.");
-    }
+    throw new Error("Method not implemented.");
+  }
 
-    async getUIDfromUsername(username: string): Promise<UID> {
-        // Get users/usernames, find given username, and return UID
-        const docRef = doc(FireDB, "users", "usernames");
-        const usernames = await getDoc(docRef);
+  editUser(user: UserEditable, uid: string): void {
+    // Update doc with the changes
+    throw new Error("Method not implemented.");
+  }
 
-        if (usernames.exists()) return usernames.data().uid;
-        else throw new Error("No usernames found");
-    }
+  async getUIDfromUsername(username: string): Promise<UID> {
+    // Get users/usernames, find given username, and return UID
+    const docRef = doc(FireDB, "users", "usernames");
+    const usernames = await getDoc(docRef);
 
-    async getAllPosts(): Promise<Post[]> {
-        // Get every post from the server
-        const postsRef = collection(FireDB, "posts");
-        const allDocs = await getDocs(postsRef);
+    if (usernames.exists()) return usernames.data().uid;
+    else throw new Error("No usernames found");
+  }
 
-        let posts: Post[] = [];
-        allDocs.forEach((doc) => {
-            posts.push(doc.data() as Post);
-        });
+  async getAllPosts(): Promise<Post[]> {
+    // Get every post from the server
+    const postsRef = collection(FireDB, "posts");
+    const allDocs = await getDocs(postsRef);
 
-        return posts;
-    }
+    let posts: Post[] = [];
+    allDocs.forEach((doc) => {
+      posts.push(doc.data() as Post);
+    });
 
-    async getPostsFromUsers(uids: string[]): Promise<Post[]> {
-        // Get a list of all posts from a list of UIDs
-        const usersRef = collection(FireDB, "users");
+    return posts;
+  }
 
-        const posts: Post[] = [];
+  async getPostsFromUsers(uids: string[]): Promise<Post[]> {
+    // Get a list of all posts from a list of UIDs
+    const usersRef = collection(FireDB, "users");
 
-        uids.forEach(async (uid) => {
-            const userDoc = await getDoc(doc(usersRef, uid));
-            if (userDoc.exists()) posts.concat(userDoc.data() as Post[]);
-        });
+    const posts: Post[] = [];
 
-        return posts;
-    }
+    uids.forEach(async (uid) => {
+      const userDoc = await getDoc(doc(usersRef, uid));
+      if (userDoc.exists()) posts.concat(userDoc.data() as Post[]);
+    });
 
-    async getAllUsernames(): Promise<any[]> {
-        // Grab all currently existing usernames, uid pairs
-        const usernamesRef = doc(collection(FireDB, "users"), "usernames");
-        const allUsernames = await getDoc(usernamesRef);
-        return allUsernames.data() as any[];
-    }
+    return posts;
+  }
+
+  async getAllUsernames(): Promise<any[]> {
+    // Grab all currently existing usernames, uid pairs
+    const usernamesRef = doc(collection(FireDB, "users"), "usernames");
+    const allUsernames = await getDoc(usernamesRef);
+    return allUsernames.data() as any[];
+  }
 }
 
 export { Database };
--- src/backend/types/interfaces.ts
+++ src/backend/types/interfaces.ts
@@ -1,41 +1,41 @@
 interface IFeedComposer {
-    composeFeed(): Promise<Feed>;
+  composeFeed(): Promise<Feed>;
 }
 
 interface IDatabase {
-    getPost(pid: PID): Promise<Post>;
-    makePost(post: Post): void;
-    editPost(post: PostEditable, pid: PID): void;
+  getPost(pid: PID): Promise<Post>;
+  makePost(post: Post): void;
+  editPost(post: PostEditable, pid: PID): void;
 
-    getUser(uid: UID): Promise<User>;
-    makeUser(user: User, uid: UID): void;
-    editUser(user: UserEditable, uid: UID): void;
+  getUser(uid: UID): Promise<User>;
+  makeUser(user: User, uid: UID): void;
+  editUser(user: UserEditable, uid: UID): void;
 
-    getUIDfromUsername(username: string): Promise<UID>;
-    getAllPosts(): Promise<Post[]>;
-    getPostsFromUsers(uids: UID[]): Promise<Post[]>;
-    getAllUsernames(): Promise<any[]>;
+  getUIDfromUsername(username: string): Promise<UID>;
+  getAllPosts(): Promise<Post[]>;
+  getPostsFromUsers(uids: UID[]): Promise<Post[]>;
+  getAllUsernames(): Promise<any[]>;
 }
 
 interface IMediaStorage {
-    getLink(path: string): Promise<string>;
-    upload(path: string, file: Blob): void;
+  getLink(path: string): Promise<string>;
+  upload(path: string, file: Blob): void;
 }
 
 interface IPostBuilder {
-    makePost(title: string, file: Blob, uid: UID, username: string): void;
-    makeReply(
-        parentPID: PID,
-        title: string,
-        file: Blob,
-        uid: UID,
-        username: string
-    ): void;
-    editLikes(pid: PID, delta: number): void;
+  makePost(title: string, file: Blob, uid: UID, username: string): void;
+  makeReply(
+    parentPID: PID,
+    title: string,
+    file: Blob,
+    uid: UID,
+    username: string
+  ): void;
+  editLikes(pid: PID, delta: number): void;
 }
 
 interface IAuthenticator {
-    signUp(username: string, email: string, password: string): UID;
-    signIn(username: string, password: string): UID;
-    signOut(): void;
+  signUp(username: string, email: string, password: string): Promise<UID>;
+  signIn(email: string, password: string): Promise<UID>;
+  signOut(): void;
 }
