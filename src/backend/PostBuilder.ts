002520e1c508b65863b190da774da4eb248de96a
Bruce Quach <42898016+bquach1@users.noreply.github.com>
2022-04-02 19:19:00 -0500

    Update makePost and makeReply functions

--- src/backend/PostBuilder.ts
+++ src/backend/PostBuilder.ts
@@ -1,10 +1,33 @@
 // TODO
 
+import { removeTypeDuplicates } from "@babel/types";
+import { Database } from "./storage/Database";
+import { MediaStorage } from "./storage/MediaStorage";
+
 class PostBuilder implements IPostBuilder {
+    database: IDatabase;
+    mediaStorage: IMediaStorage; 
+
+    constructor() {
+        this.database = new Database();
+        this.mediaStorage = new MediaStorage();
+    }
+
     makePost(title: string, file: Blob, uid: string, username: string): void {
         // Call MediaStorage.upload and MediaStorage.getLink.
         // Create a Post object, then call Database.makePost.
-        throw new Error("Method not implemented.");
+        // likes are 0, replies are an empty array, create a timestamp
+        likes: 0;
+        replies: Array<Post>();
+        timestamp: 0;
+
+        this.mediaStorage.upload(this.mediaStorage.path, this.mediaStorage.file);
+        this.mediaStorage.getLink(this.mediaStorage.path, this.mediaStorage.file);
+
+        post: Database.getPost(title);
+        this.database.makePost(post);
+
+        // throw new Error("Method not implemented.");
     }
 
     makeReply(
@@ -16,14 +39,20 @@ class PostBuilder implements IPostBuilder {
     ): void {
         // Call makePost, then append reply
         // to parent post's replies list.
-        throw new Error("Method not implemented.");
+        
+        replies.push(this.database.makePost(post));
+        
+        // throw new Error("Method not implemented.");
     }
 
     editLikes(uid: UID, pid: PID, delta: number): void {
         // Get likes from database,
-        // Update likes with likes + delta
-        // Append pid to likedPosts on user
-        throw new Error("Method not implemented.");
+        // update likes with likes + delta
+        // create post editable object, create object with just likes +/- 1
+        
+        
+
+        // throw new Error("Method not implemented.");
     }
 }
 
