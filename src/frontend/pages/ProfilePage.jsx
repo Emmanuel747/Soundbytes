67ae5e02362a8ed005f66673920efd0ecd34fa23
mseruntine <miguelseruntine@gmail.com>
2022-04-03 20:21:18 -0500

    profile base

--- src/frontend/pages/ProfilePage.jsx
+++ src/frontend/pages/ProfilePage.jsx
@@ -1,10 +1,15 @@
-import { useContext } from "react";
+import { useContext, } from "react";
 import { useParams } from "react-router";
 import { ProfileFeedComposer, Database } from "../../backend";
 import { Feed, IconButton, TextInput } from "../components";
 import { UserContext } from "../hooks/UserContext";
 import useProtectedRoute from "../hooks/useProtectedRoute";
 import useAsync from "../hooks/useAsync";
+import React, { useState, useEffect } from "react";
+
+import { IconName } from "react-icons/ai";
+
+import '../Styles/ProfilePage.scss';
 
 const SearchBar = () => {
     const getAllUsernames = () => {
@@ -14,19 +19,39 @@ const SearchBar = () => {
     return (
         <div>
             <TextInput placeHolder='Search Users' />
-            <IconButton icon='' />
+            <IconName AiFillHome='' />
         </div>
     );
 };
 
 const YourProfile = ({ user }) => {
-    return (
-        <>
-            <h3>{"<YOUR USERNAME>"}</h3>
-            <h4>{"<BIO?>"}</h4>
-            <Feed feedFactory={new ProfileFeedComposer()} />
-        </>
-    );
+	return (
+        
+		<div className="headerContainer flex flex-col">
+			<img className="userProfileImg self-center"
+                    style={{maxWidth: "10rem"}}
+					src="https://cdn.discordapp.com/attachments/178196683727962112/953082990806831176/unknown.png"
+			/>
+			<div className="profileContainer text-center flex-col justify-center items-center">    
+				<h3>{"Badmamajama420"}</h3> 
+				<h4>{"\"I make great life decisions\""}</h4>
+					
+			</div>
+            <div className="followerContainer flex flex-row self-center"> 
+            <p onClick={() => {
+                //displayAllFollowers(); 
+                //displayAllFollowers() is a function that could theoretically show a dropdown list of your followers
+            }}> Followers: {50}
+            </p> 
+            <p onClick={() => {
+                //displayAllFollowing();
+                //displayAllFollowing(); is a function that could theoretically show a dropdown list of your followers
+            }}> Following: {17} 
+            </p>
+            </div>
+            <Feed feedFactory={new ProfileFeedComposer()} />         
+		</div>
+	);
 };
 
 const OtherProfile = ({ username, uid }) => {
@@ -55,7 +80,6 @@ export default function ProfilePage() {
     return (
         <div>
             <SearchBar />
-            <h1>Profile</h1>
             {username ? (
                 <OtherProfile username={username} uid={uid} />
             ) : (
