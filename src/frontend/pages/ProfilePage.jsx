c16152675bcceffcc2b289a4bf16a0c9200d39bf
mseruntine <miguelseruntine@gmail.com>
2022-04-03 22:27:03 -0500

    profile page version 0.2

--- src/frontend/pages/ProfilePage.jsx
+++ src/frontend/pages/ProfilePage.jsx
@@ -1,10 +1,15 @@
-import { useContext } from "react";
+import { useContext, } from "react";
 import { useParams } from "react-router";
 import { ProfileFeedComposer, Database } from "../../backend";
 import { Feed, IconButton, TextInput } from "../components";
 import { UserContext } from "../hooks/UserContext";
 import useProtectedRoute from "../hooks/useProtectedRoute";
 import useAsync from "../hooks/useAsync";
+import React, { useState, useEffect } from "react";
+
+import { AiFillHome } from "react-icons/ai";
+
+import '../Styles/ProfilePage.scss';
 
 const SearchBar = () => {
     const getAllUsernames = () => {
@@ -13,20 +18,42 @@ const SearchBar = () => {
 
     return (
         <div>
-            <TextInput placeHolder='Search Users' />
-            <IconButton icon='' />
+            <TextInput
+                style={{border: '10px',borderStyle:'solid',borderWidth: 1,borderColor: 'black',  //This styling does not work, but I am
+                borderRadius: 5}}                                                                //too tired to figure that out tonight.
+                placeHolder='Search Users' />                                                    
+            <AiFillHome 
+                style={{fontSize:'64px'}} />
         </div>
     );
 };
 
 const YourProfile = ({ user }) => {
-    return (
-        <>
-            <h3>{"<YOUR USERNAME>"}</h3>
-            <h4>{"<BIO?>"}</h4>
-            <Feed feedFactory={new ProfileFeedComposer()} />
-        </>
-    );
+	return (
+        
+		<div className="headerContainer flex flex-col">
+			<img className="userProfileImg self-center"
+                    style={{maxWidth: "10rem"}}
+					src="https://cdn.discordapp.com/attachments/178196683727962112/953082990806831176/unknown.png"
+			/>
+			<div className="profileContainer text-center flex-col justify-center items-center">    
+				<h3>{"Badmamajama420"}</h3> 
+				<h4>{"\"I make great life decisions\""}</h4>
+					
+			</div>
+            <div className="followerContainer flex justify-around"> 
+            <p onClick={() => {
+                //implement displayAllFollowers(); 
+            }}> Followers: {50}
+            </p> 
+            <p onClick={() => {
+                //implement displayAllFollowing();
+            }}> Following: {17} 
+            </p>
+            </div>
+            <Feed feedFactory={new ProfileFeedComposer()} />         
+		</div>
+	);
 };
 
 const OtherProfile = ({ username, uid }) => {
@@ -55,7 +82,6 @@ export default function ProfilePage() {
     return (
         <div>
             <SearchBar />
-            <h1>Profile</h1>
             {username ? (
                 <OtherProfile username={username} uid={uid} />
             ) : (
