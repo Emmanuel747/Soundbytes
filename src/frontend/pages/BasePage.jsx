dd434a25b6aadb315f4c372d7e8d8f215c0805da
Zeke <zekeabshire@gmail.com>
2022-04-04 19:36:56 -0500

    removed comment

--- src/frontend/pages/BasePage.jsx
+++ src/frontend/pages/BasePage.jsx
@@ -2,17 +2,12 @@ import { Outlet } from "react-router";
 import { Navbar } from "../components";
 import useProtectedRoute from "../hooks/useProtectedRoute";
 import { UserContext } from "../hooks/UserContext";
-import { useContext, useEffect, useState } from "react";
+import { useContext } from "react";
 
 export default function BasePage() {
     useProtectedRoute();
 
     const { currentUsername } = useContext(UserContext);
-    const [username, setUsername] = useState(currentUsername);
-
-    // useEffect(() => {
-    //     setUsername(currentUsername);
-    // }, [currentUsername]);
 
     return currentUsername ? (
         <>
