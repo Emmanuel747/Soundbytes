33bf3ac9896fadaaa4c70f0db5295e24abd1f9f7
Zeke <zekeabshire@gmail.com>
2022-04-04 20:04:48 -0500

    slightly upgraded post component

--- src/frontend/components/Post.jsx
+++ src/frontend/components/Post.jsx
@@ -1,11 +1,12 @@
 export default function Post({ post }) {
     return (
-        <div className='bg-slate-300 border border-slate-600 p-4 '>
-            <h4 className="text-center">{"<POST TITLE>"}</h4>
-            <p>{"<USERNAME>"}</p>
-            <p>{"<TIMESTAMP>"}</p>
-            <p>{"<LIKES>"}</p>
-            <audio src='#' controls='controls'></audio>
+        <div className='p-4 border bg-slate-300 border-slate-600 '>
+            <h4 className='text-center'>{post.title}</h4>
+            <p>{post.uid}</p>
+            <p>{post.timestamp}</p>
+            <p>{post.likes}</p>
+            {/* <p>{post.replies}</p> */}
+            <audio src={post.audioURL} controls='controls'></audio>
         </div>
     );
 }
