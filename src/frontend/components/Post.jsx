1b877bc0bdf0e1ed29038512aa9e8408e5030753
FyreDrake4 <44129311+FyreDrake4@users.noreply.github.com>
2022-04-05 17:34:53 -0500

    small changes

--- src/frontend/components/Post.jsx
+++ src/frontend/components/Post.jsx
@@ -1,12 +1,23 @@
+import { Database } from "../../backend";
+import useAsync from "../hooks/useAsync";
+
 export default function Post({ post }) {
+    const date = new Date(parseInt(post.timestamp));
+    /*const genUser = async () => {
+        const currUser = await new Database().getUser(post.uid);
+        return currUser.map((userData) => <User user={userData} />);
+    };
+    const postUser = useAsync(genUser); tried to do something similar to feed but nope*/
     return (
-        <div className='p-4 border bg-slate-300 border-slate-600 '>
-            <h4 className='text-center'>{post.title}</h4>
-            <p>{post.uid}</p>
-            <p>{post.timestamp}</p>
-            <p>{post.likes}</p>
-            {/* <p>{post.replies}</p> */}
-            <audio src={post.audioURL} controls='controls'></audio>
+        <div className="bg-slate-300 border border-slate-600 p-4 ">
+            <h4 className="text-center">{post.title}</h4>
+            <p>{"Username"}</p>
+            <p>{date.toLocaleDateString("en-US")}</p>
+            <p>
+                {"Likes: "}
+                {post.likes}
+            </p>
+            <audio src="#" controls="controls"></audio>
         </div>
     );
 }
