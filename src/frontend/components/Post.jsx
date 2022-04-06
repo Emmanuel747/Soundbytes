9cc169c443bf0a892d2ddcb5da86f92f09cfca59
Zeke <zekeabshire@gmail.com>
2022-04-06 11:04:05 -0500

    set up most of post component

--- src/frontend/components/Post.jsx
+++ src/frontend/components/Post.jsx
@@ -1,43 +1,69 @@
 import { Database } from "../../backend";
-import useAsync from "../hooks/useAsync";
 import { AiFillLike, AiOutlineLike } from "react-icons/ai";
+import { UserContext } from "../hooks/UserContext";
+import { useState, useContext, useEffect } from "react";
 
 export default function Post({ post }) {
-    const date = new Date(parseInt(post.timestamp));
- 
+    const { currentUID } = useContext(UserContext);
+    const [user, setUser] = useState();
+    const [likedPost, setLikedPost] = useState(false);
+    const [otherUsername, setOtherUsername] = useState("");
+    const [otherPFP, setOtherPFP] = useState("");
 
-    const HandleLikes = () => {
-        // new Database().editPost(post, post)
-        // console.log(post);
-        // post.likes = post.likes + 1;
-        console.log(post.uid)
-        let user = new Database.getUser(post.uid);
-        console.log(user)
-    }
-    console.log(post);
-    // audioURL: 'https://firebasestorage.googleapis.com/v0/b/notify…=media&token=c806eb71-0d63-428c-bc58-cf9ee3e9857a', replies: Array(0), uid: 'WM7kGbC1u0gBctjWeg3FFoxdGjJ2', title: '', likes: 0, …}
-    //   audioURL: "https://firebasestorage.googleapis.com/v0/b/notify-soundbytes.appspot.com/o/sounds%2FWM7kGbC1u0gBctjWeg3FFoxdGjJ2-1649120941153?alt=media&token=c806eb71-0d63-428c-bc58-cf9ee3e9857a"
-    //   likes: 0
-    //   replies: Array(0)
-    //   length: 0
-    //   [[Prototype]]: Array(0)
-    //   timestamp: "1649120941153"
-    //   title: ""
-    //   uid: "WM7kGbC1u0gBctjWeg3FFoxdGjJ2"
+    const getTimestamp = () =>
+        new Date(parseInt(post.timestamp)).toLocaleDateString("en-US");
+
+    const getUser = async () => {
+        const user = await new Database.getUser(currentUID);
+        setUser(user);
+
+        const tempPID = "";
+
+        // Set the like indicator to the correct one
+        setLikedPost(user.likedPosts.includes(tempPID));
+    };
+
+    const getOtherUser = async () => {
+        const other = await new Database.getUser(post.uid);
+        setOtherPFP(other.pfpURL);
+        setOtherUsername(other.username);
+    };
+
+    const handleLikes = async () => {
+        const db = new Database();
+        const tempPID = "";
+
+        // See if the user had already liked it
+        // TODO
+
+        // Update user's liked posts
+        const currentLikes = user.likedPosts;
+        currentLikes.push(tempPID);
+
+        await db.editUser({ likedPosts: currentLikes }, currentUID);
+    };
+
+    useEffect(() => {
+        getUser();
+        getOtherUser();
+    }, []);
 
     return (
-        <div className="bg-slate-300 border border-slate-600 p-4 ">
-            <h4 className="text-center">{post.title}</h4>
-            <p className="username">{}</p>
-            <p>{date.toLocaleDateString("en-US")}</p>
+        <div className='p-4 border bg-slate-300 border-slate-600 '>
+            <h4 className='text-center'>{post.title}</h4>
+            <p className='username'>{otherUsername}</p>
+            {/* <img src={otherPFP} alt="profile picture" /> */}
+            <p>{getTimestamp()}</p>
             <p>
-                <div onClick={() =>{HandleLikes()}}>
-                {<AiOutlineLike style={{ fontSize: "32px" }}  />}
-                {post.likes}
+                <div
+                    onClick={() => {
+                        handleLikes();
+                    }}>
+                    {<AiOutlineLike style={{ fontSize: "32px" }} />}
+                    {post.likes}
                 </div>
-                
             </p>
-            <audio src={post.audioURL} controls="controls"></audio>
+            <audio src={post.audioURL} controls></audio>
         </div>
     );
 }
