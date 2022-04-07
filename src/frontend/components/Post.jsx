8a085dc8465384a1daa08a621446627b9785cc59
Zeke <zekeabshire@gmail.com>
2022-04-06 20:55:49 -0500

    added post like button

--- src/frontend/components/Post.jsx
+++ src/frontend/components/Post.jsx
@@ -7,6 +7,7 @@ export default function Post({ post }) {
     const { currentUID } = useContext(UserContext);
     const [user, setUser] = useState();
     const [likedPost, setLikedPost] = useState(false);
+    const [likeCount, setLikeCount] = useState(0);
     const [otherUsername, setOtherUsername] = useState("");
     const [otherPFP, setOtherPFP] = useState("");
 
@@ -17,10 +18,8 @@ export default function Post({ post }) {
         const user = await new Database().getUser(currentUID);
         setUser(user);
 
-        const tempPID = "";
-
         // Set the like indicator to the correct one
-        setLikedPost(user.likedPosts.includes(tempPID));
+        setLikedPost(user.likedPosts.includes(post.pid));
     };
 
     const getOtherUser = async () => {
@@ -31,35 +30,58 @@ export default function Post({ post }) {
 
     const handleLikes = async () => {
         const db = new Database();
-        const tempPID = "";
 
         // See if the user had already liked it
-        // TODO
+        const currentLikes = user.likedPosts;
+        let delta = 0;
+
+        if (currentLikes.includes(post.pid)) {
+            // Post is already liked, so unlike it
+            delta--;
+            setLikedPost(false);
+            currentLikes.splice(currentLikes.indexOf(post.pid), 1);
+        } else {
+            // Post not liked yet, so like it
+            setLikedPost(true);
+            delta++;
+            currentLikes.push(post.pid);
+        }
+
+        const currentLikeCount = likeCount + delta;
+
+        setLikeCount(currentLikeCount);
+        console.log("Likes", currentLikeCount);
 
         // Update user's liked posts
-        const currentLikes = user.likedPosts;
-        currentLikes.push(tempPID);
-
         await db.editUser({ likedPosts: currentLikes }, currentUID);
+        await db.editPost({ likes: delta }, post.pid, post.uid);
     };
 
     useEffect(() => {
         getUser();
         getOtherUser();
+        setLikeCount(post.likes);
     }, []);
 
     return (
         <div className='p-4 border bg-slate-300 border-slate-600 '>
             <h4 className='text-center'>{post.title}</h4>
             <p className='username'>{otherUsername}</p>
-            {/* <img src={otherPFP} alt="profile picture" /> */}
+            <img
+                style={{ width: 80, height: 80 }}
+                src={otherPFP}
+                alt='Profile'
+            />
             <p>{getTimestamp()}</p>
             <div
                 onClick={() => {
                     handleLikes();
                 }}>
-                {<AiOutlineLike style={{ fontSize: "32px" }} />}
-                <p>{post.likes}</p>
+                {/* <AiOutlineLike style={{ fontSize: "32px" }} /> */}
+                <div onClick={() => setLikedPost(!likedPost)}>
+                    {likedPost ? "LIKED" : "NOT LIKED"}
+                </div>
+                <p>{likeCount}</p>
             </div>
             <audio src={post.audioURL} controls></audio>
         </div>
